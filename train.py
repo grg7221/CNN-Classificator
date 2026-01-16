2ac6c4b7601f26f92b903826a1b3b50e02374181
grg7221 <kopchenoe.salo@inbox.ru>
2026-01-16 17:26:56 +0300

    train/val split, manual seed, val metrics

--- train.py
+++ train.py
@@ -3,20 +3,35 @@ from torch.optim import Adam
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from tqdm import trange
+#from tqdm import trange
+
+torch.manual_seed(2506)
 
 batch_size = 512
 vocab_size = 25000
-embed_dim = 100
+embed_dim = 128
 num_filters = 100
 kernel_sizes = [3, 4, 5]
 epochs = 10
+val_split = 0.1
 lr = 1e-3 * (batch_size / 512)
 
-data = torch.load('dataset/IMDB/train/train.pt')
-X = data[0].cuda().long()
-Y = data[1].cuda().long()
+data = torch.load('dataset/IMDB/train/train.pt', map_location='cuda')
+X = data[0].long()
+Y = data[1].long()
 N = len(X)
+
+perm = torch.randperm(N, device='cuda')
+X = X[perm]
+Y = Y[perm]
+
+split = int(N*val_split)
+N -= split
+X_train = X[split:]
+Y_train = Y[split:]
+
+X_val = X[:split]
+Y_val = Y[:split]
 
 model = CNN(
     vocab_size=vocab_size,
@@ -33,12 +48,13 @@ print(f"Total params: {sum(p.numel() for p in model.parameters())}")
 for e in range(epochs):
     perm = torch.randperm(N, device='cuda')
 
-    X_shuffle = X[perm]
-    Y_shuffle = Y[perm]
+    X_shuffle = X_train[perm]
+    Y_shuffle = Y_train[perm]
 
     total_loss = 0
 
-    for i in trange(0, N, batch_size):
+    # train split
+    for i in range(0, N, batch_size):
         xb = X_shuffle[i:i+batch_size]
         yb = Y_shuffle[i:i+batch_size]
 
@@ -51,4 +67,22 @@ for e in range(epochs):
 
         total_loss += loss.item()
 
-    print(f"Epoch {e+1}: total epoch loss={total_loss:.4f}, last loss={loss.item():.4f}")
+    TP = FP = FN = TN = 0
+
+    # val split
+    for i in range(0, len(X_val), batch_size):
+        logits = model(X_val[i:i+batch_size])
+        val_loss = F.cross_entropy(logits, Y_val[i:i+batch_size])
+        preds = logits.argmax(dim=1)
+        y = Y_val[i:i+batch_size]
+
+        TP += ((preds == 1) & (y == 1)).sum().item()
+        FP += ((preds == 1) & (y == 0)).sum().item()
+        FN += ((preds == 0) & (y == 1)).sum().item()
+        TN += ((preds == 0) & (y == 0)).sum().item()
+
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    precision = TP / (TP + FP + 1e-8)
+    recall = TP / (TP + FN + 1e-8)
+    print(f"Epoch {e+1}: total_loss={total_loss:.4f}, last loss={loss.item():.4f}")
+    print(f"Val split: val_loss={val_loss:.4f} acc={accuracy:.4f}, precision={precision:.4f}, recall={recall:.4f}")
